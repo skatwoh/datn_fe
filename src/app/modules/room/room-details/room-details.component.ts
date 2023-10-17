90243444729aeabf285f0f0bb6ba29e6da0fbabc
khanhdq <khanhdq2411@gmail.com>
2023-10-17 08:34:09 +0700

    tao view detail cho room

--- src/app/modules/room/room-details/room-details.component.ts
+++ src/app/modules/room/room-details/room-details.component.ts
@@ -1,23 +1,25 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute, Route, Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 import {RoomService} from "../services/room.service";
 import {RoomModel} from "../../../models/room.model";
+import {RoomDTOModel} from "../../../models/roomDTO.model";
 
 @Component({
   selector: 'cons-room-details',
   templateUrl: './room-details.component.html',
-  styleUrls: ['./room-details.component.scss']
+  styleUrls: ['./room-details.component.scss'],
 })
-export class RoomDetailsComponent implements OnInit{
- id : number | undefined;
- room! : RoomModel;
- constructor(public roomService : RoomService, private router : Router, private route : ActivatedRoute) {
+export class RoomDetailsComponent implements OnInit {
+  id: number | undefined;
+  room!: RoomDTOModel;
 
- }
- ngOnInit() {
-   this.id = this.route.snapshot.params['id'];
-   this.roomService.get(this.id).subscribe((data: RoomModel) => {
-     this.room = data;
-   });
- }
+  constructor(public roomService: RoomService, private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.roomService.get(this.id).subscribe((data: RoomDTOModel) => {
+      this.room = data;
+      console.log(this.room);
+    });
+  }
 }
