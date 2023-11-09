43c29159a047c7bc13bf0c39f7c7a9574285895a
khanhdq <khanhdq2411@gmail.com>
2023-11-09 21:41:54 +0700

    update booking

--- src/app/web/index/page/room-details/room-details.component.ts
+++ src/app/web/index/page/room-details/room-details.component.ts
@@ -1,25 +1,23 @@
-import {Component, Input, OnInit, TemplateRef} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {RoomTypeDtoModel} from "../../../../models/room-type-dto.model";
 import {ActivatedRoute, Router} from "@angular/router";
 import {RoomInformationModel} from "../../../../models/room-information.model";
 import {RoomInformationService} from "../../../../modules/room-details/services/room-information.service";
-import {Observable} from "rxjs";
+import {first, Observable, Subscription} from "rxjs";
 import {AuthService} from "../../../../auth/services";
 import {RoomManagerService} from "../../../../modules/room-manager/services/room-manager.service";
 import {UserModel} from "../../../../auth/models/user.model";
 import {RoomService} from "../../../../modules/room/services/room.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {RoomModel} from "../../../../models/room.model";
-import {formatCurrency, getCurrencySymbol, getLocaleCurrencyName} from "@angular/common";
-import {vi_VN} from "ng-zorro-antd/i18n";
 import {NzNotificationService} from "ng-zorro-antd/notification";
+import {AppConstants} from "../../../../app-constants";
 
 @Component({
   selector: 'cons-room-details',
   templateUrl: './room-details.component.html',
   styleUrls: ['./room-details.component.scss']
 })
-export class RoomDetailsComponent implements OnInit {
+export class RoomDetailsComponent implements OnInit, OnDestroy {
   user$: Observable<any>;
   idPhong: number | undefined;
   room!: RoomInformationModel;
@@ -27,7 +25,9 @@ export class RoomDetailsComponent implements OnInit {
   message = '';
   user: UserModel | undefined;
   roomOrderForm: FormGroup;
+  hasError = false;
   submitted = false;
+  private unsubscribe: Subscription[] = [];
 
   constructor(public roomService: RoomInformationService, private router: Router, private route: ActivatedRoute,
               private roomService1: RoomService, private authService: AuthService, private roomManagerService: RoomManagerService,
@@ -65,19 +65,37 @@ export class RoomDetailsComponent implements OnInit {
   }
 
   saveRoomOrder(): void {
+    if(this.user?.name == null){
+      this.router.navigate(['/hotel/login']);
+    }
+    this.hasError = false;
     if (this.roomOrderForm.valid) {
       const data = this.roomOrderForm.value;
 
-      this.roomManagerService.create(data).subscribe({
-        next: (res) => {
-          console.log(res);
-          this.submitted = true;
-          this.messSuccess();
-          this.router.navigate(['/'])
+      const sub = this.roomManagerService.create(data)
+        .pipe(first())
+        .subscribe((res) => {
+          if (res?.code === AppConstants.API_SUCCESS_CODE){
+            this.submitted = true;
+            this.messSuccess();
+            this.router.navigate(['/']);
+          } else {
+            if (res?.code === AppConstants.API_BAD_REQUEST_CODE && res?.entityMessages.length > 0) {
+              const msg: any = res.entityMessages[0];
+              this.notification.warning(`${msg.errorMessage}`, "");
+            } else {
+              this.message = `Error`;
+            }
+            this.hasError = true;
+          }
         },
-        error: (e) => console.error(e)
-      })
+      );
+      this.unsubscribe.push(sub);
     }
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.forEach((sb) => sb.unsubscribe());
   }
 
   protected readonly Number = Number;
