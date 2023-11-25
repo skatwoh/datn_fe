c78020c9993eb4d9a5a615ce45c31798cff50216
HOANGLONG2303\longh <longhoang0323@gmail.com>
2023-11-25 20:36:45 +0700

    update search room

--- src/app/web/index/page/list-room-order/list-room-order.component.ts
+++ src/app/web/index/page/list-room-order/list-room-order.component.ts
@@ -9,6 +9,7 @@ import {RoomModel} from "../../../../models/room.model";
 import {NzMessageService} from "ng-zorro-antd/message";
 import {AppConstants} from "../../../../app-constants";
 import {NzNotificationService} from "ng-zorro-antd/notification";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'cons-list-room-order',
@@ -20,18 +21,21 @@ export class ListRoomOrderComponent implements OnInit, OnDestroy {
   room1: RoomModel[] = [];
 
   currentRoom!: RoomOrder;
+  currentRoom2!: RoomOrder;
   user: UserModel | undefined;
   isVisible = false;
   isOkLoading = false;
   isVisible1 = false;
   isOkLoading1 = false;
   id : number | undefined;
+  id2 : number | undefined;
   message2 : string = '';
   hasError = false;
   constructor(private roomOrderService: ListRoomOrderService,
               private message: NzMessageService,
               private authService: AuthService,
-              private notification: NzNotificationService) {
+              private notification: NzNotificationService,
+              private router: Router) {
     this.user = authService.currentUserValue;
   }
 
@@ -43,8 +47,13 @@ export class ListRoomOrderComponent implements OnInit, OnDestroy {
       console.log(this.currentRoom);
     });
   }
-  showRoomUpperPrice(giaPhong: number | undefined): void {
+  showRoomUpperPrice(giaPhong: number | undefined, id: any): void {
     this.isVisible1 = true;
+    this.id = id;
+    this.roomOrderService.get(this.id).subscribe((data: RoomOrder) => {
+      this.currentRoom = data;
+      console.log(this.currentRoom);
+    });
     this.roomOrderService.getListRoomByUpperPrice(1, 50, giaPhong ).subscribe(res => {
       if (res && res.content) {
         this.room1 = res.content;
@@ -103,6 +112,14 @@ export class ListRoomOrderComponent implements OnInit, OnDestroy {
           this.room = res.content;
         }
       })
+  }
+
+  // getRoom2(id: any) {
+  //   this.router.navigate([`/room-order-change/${id}`]);
+  // }
+
+  handleCancel1(): void {
+    this.isVisible1 = false;
   }
 
   ngOnInit(): void {
