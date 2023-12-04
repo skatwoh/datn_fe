5713c815000e603a709122397aaff98e73c8c266
DESKTOP-T0368G0\fuoc <nxp132003@gmail.com>
2023-12-04 14:13:45 +0700

    update bao tri, doi tac

--- src/app/modules/maintenance/maintenance-create/maintenance-create.component.ts
+++ src/app/modules/maintenance/maintenance-create/maintenance-create.component.ts
@@ -1,33 +1,39 @@
-import {Component} from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {NzMessageService} from "ng-zorro-antd/message";
 import {environment} from "../../../../environments/environment";
 import {MaintenanceModel} from "../../../models/maintenance.model";
 import {RoomInformationModel} from "../../../models/room-information.model";
 import {MaintenanceService} from "../service/maintenance.service";
+import {Router} from "@angular/router";
+import {first, Subscription} from "rxjs";
+import {AppConstants} from "../../../app-constants";
+import {NzNotificationService} from "ng-zorro-antd/notification";
 
 @Component({
   selector: 'cons-maintenance-create',
   templateUrl: './maintenance-create.component.html',
   styleUrls: ['./maintenance-create.component.scss']
 })
-export class MaintenanceCreateComponent {
-
-  maintenance : MaintenanceModel = {
-    id: 0,
-    idChiTietPhong: 0,
-    ngayBatDau: '',
-    ngayKetThuc: '',
-    chiPhiBaoTri:0,
-    ghiChu: '',
-    trangThai: 0
-  };
+export class MaintenanceCreateComponent implements OnInit{
+  maintenanceForm: FormGroup;
+message1: string = '';
   roomInfor : RoomInformationModel[] = [];
   submitted = false;
   hasError: boolean = false;
 
-  constructor(private maintenanceService: MaintenanceService, private http : HttpClient, private message: NzMessageService,  private fb: FormBuilder) {}
+  private unsubscribe: Subscription[] = [];
+
+  constructor(private maintenanceService: MaintenanceService, private http : HttpClient, private message: NzMessageService,  private fb: FormBuilder,private router: Router,private notification:NzNotificationService) {
+    this.maintenanceForm = this.fb.group({
+    ngayBatDau: ['', Validators.required],
+    ngayKetThuc: ['', Validators.required],
+    ghiChu: [0, Validators.required],
+    chiPhiBaoTri: new FormControl(null, Validators.compose([Validators.nullValidator, Validators.min(1000), Validators.max(100000000000)])),
+    trangThai: 1,
+    idChiTietPhong: ['', Validators.required]
+  });}
 
   ngOnInit() {
 
@@ -43,36 +49,33 @@ export class MaintenanceCreateComponent {
   }
 
   saveMaintenance(): void {
-    const data = {
-      ngayBatDau: this.maintenance.ngayBatDau,
-      ngayKetThuc: this.maintenance.ngayKetThuc,
-      chiPhiBaoTri: this.maintenance.chiPhiBaoTri,
-      ghiChu: this.maintenance.ghiChu,
-      trangThai: 1,
-      idChiTietPhong: this.maintenance.idChiTietPhong
-    };
+    if (this.maintenanceForm.valid) {
+      const data = this.maintenanceForm.value;
 
-    this.maintenanceService.create(data).subscribe({
-      next: (res) => {
-        this.successMessage();
-        this.submitted = true;
-      },
-      error: (e) => console.error(e)
-    });
+      const sub = this.maintenanceService.create(data)
+        .pipe(first())
+        .subscribe((res) => {
+            if (res?.code === AppConstants.API_SUCCESS_CODE){
+              this.submitted = true;
+              this.successMessage();
+              this.router.navigate(['/admin/maintenance']);
+              // this.showModal();
+            } else {
+              if (res?.code === AppConstants.API_BAD_REQUEST_CODE && res?.entityMessages.length > 0) {
+                const msg: any = res.entityMessages[0];
+                this.notification.warning(`${msg.errorMessage}`, "");
+              } else {
+                this.message1 = `Error`;
+              }
+              this.hasError = true;
+            }
+          },
+        );
+      this.unsubscribe.push(sub);
+    }
   }
 
-  newMaintenance(): void {
-    this.submitted = false;
-    this.maintenance = {
-      id: 0,
-      idChiTietPhong: 0,
-      ngayBatDau: '',
-      ngayKetThuc: '',
-      chiPhiBaoTri:0,
-      ghiChu: '',
-      trangThai: 0
-    };
-  }
+
 
   protected readonly maintenanceModel = MaintenanceModel;
 }
