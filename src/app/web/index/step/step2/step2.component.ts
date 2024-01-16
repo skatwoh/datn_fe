3a3d3053580ccd8d9bd2d6858f050c9cf59d96e7
khanhdq <khanhdq2411@gmail.com>
2024-01-16 23:39:14 +0700

    fix bug

--- src/app/web/index/step/step2/step2.component.ts
+++ src/app/web/index/step/step2/step2.component.ts
@@ -17,17 +17,23 @@ export class Step2Component implements OnInit, OnDestroy {
   countdown: number = 180;
   currentBill!: BillModel;
   user: UserModel | undefined;
+  private intervalId: any;
 
   constructor(private router: Router, private message: NzMessageService, private billService: BillService,
               private authService: AuthService, private roomOrderService: ListRoomOrderService) {
   }
 
-  ngOnDestroy(): void {
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    // Check if the pressed key is F5
+    if (event.key === 'F5' && event.ctrlKey) {
+      event.preventDefault(); // Prevent the default behavior (refreshing the page)
+    }
   }
 
   ngOnInit() {
     this.user = this.authService.currentUserValue;
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
       this.countdown--;
       if (this.countdown === 60) {
         this.message.warning("Vui lòng thanh toán để xác nhận!");
@@ -66,6 +72,26 @@ export class Step2Component implements OnInit, OnDestroy {
             console.log(res);
           },
         })
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+    const id = this.user?.id;
+    this.roomOrderService.getRoomOfBill(1, 50, id).subscribe(res => {
+      if (res && res.content) {
+        this.billService.get(res.content[0].idHoaDon).subscribe((data: BillModel) => {
+          this.currentBill = data;
+          if(data.trangThai == 1){
+            this.billService.updateStatus(res.content[0].idHoaDon, 4).subscribe({
+              next: (res) => {
+                this.currentBill.trangThai = 4;
+                console.log(res);
+              },
+            })
+          }
+        });
       }
     })
   }
